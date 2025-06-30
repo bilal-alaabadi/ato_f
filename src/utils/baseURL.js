3ad62bc7973e20ffb56f1ac250db40b6c05c74b0
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-06-30 20:48:30 +0400

    وصف التغييرات هنا

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,5 +2,5 @@
 //     return 'https://em-b-w79u.vercel.app';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+    return 'https://ato-b.vercel.app';//رابط الباك اند بدل 5000
 }
