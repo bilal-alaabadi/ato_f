7898e15e83198835908be7def53534c802dfdde3
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-07-02 21:44:29 +0400

    ??? ????????? ???

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,6 @@
-
+// export const getBaseUrl = () => {
+//     return 'https://em-b-w79u.vercel.app';//رابط الباك اند بدل 5000
+// }
 export const getBaseUrl = () => {
-    return 'https://www.octopustourism.com';//رابط الباك اند بدل 5000
+    return 'https://ato-b.vercel.app';//رابط الباك اند بدل 5000
 }
