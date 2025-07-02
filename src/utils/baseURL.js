484a0152ac3bd7476d7c10598eb566f3ac5446d6
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-07-02 21:11:09 +0400

    وصف التغييرات هنا

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,4 @@
-// export const getBaseUrl = () => {
-//     return 'https://em-b-w79u.vercel.app';//رابط الباك اند بدل 5000
-// }
+
 export const getBaseUrl = () => {
-    return 'https://ato-b.vercel.app';//رابط الباك اند بدل 5000
+    return 'https://www.octopustourism.com';//رابط الباك اند بدل 5000
 }
